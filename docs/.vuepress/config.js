6345b974994fb815e6ed075eaa0ee3c509fd865f
Ske11 <skzhao11@mail.ustc.edu.cn>
2022-08-29 12:47:44 +0800

    :tada: 开始啦!

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  base: '/deploy-demo/',
+  title: 'Ske11',
+  description: 'Ske11 Demo',
+  head: ['link', { rel: 'icon', href: '/logo.png'}], 
+  themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      {text: 'Home', link: '/'},
+      {text: '语言', 
+      ariaLabel: 'Language Menu', 
+      items: [
+        {text: '中文',link: '/language/chinese/'},
+        {text: '英文',link: '/language/english/'}
+      ]},
+    ]
+  }
+}
